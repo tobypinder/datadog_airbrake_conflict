1db32bcf2436f00cc6e4cdad85e65b4b95718998
Toby Pinder <toby@creditkudos.com>
2019-07-10 10:14:26 +0100

    Number for simplicity.

--- config/initializers/02_datadog_tracer.rb
+++ config/initializers/02_datadog_tracer.rb
@@ -5,7 +5,5 @@ Datadog.configure do |c|
   c.use :http, service_name: 'datadog_airbrake_conflict'
 
   c.tracer enabled: true,
-    debug: true,
-    hostname: '172.17.0.1',
-    port: 8126
+    debug: true
 end
